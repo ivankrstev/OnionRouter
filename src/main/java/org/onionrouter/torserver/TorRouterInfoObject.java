9ce715e9fa4c433efb348803bc3269029860965c
Ivan Krstev <ivankrstev246@gmail.com>
2024-05-19 16:42:04 +0200

    Added a function for parsing Public Key from string

--- src/main/java/org/onionrouter/torserver/TorRouterInfoObject.java
+++ src/main/java/org/onionrouter/torserver/TorRouterInfoObject.java
@@ -2,6 +2,13 @@ package org.onionrouter.torserver;
 
 import org.onionrouter.nodes.RouterStatus;
 
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
 public class TorRouterInfoObject {
     private String publicKey;
     private int port;
@@ -27,6 +34,17 @@ public class TorRouterInfoObject {
         // which contains from the generated port and router status
         this.port = port;
         this.routerStatus = routerStatus;
+    }
+
+    public PublicKey parseAndGetPublicKey() {
+        try {
+            byte[] decodedKey = Base64.getDecoder().decode(publicKey);
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decodedKey);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return keyFactory.generatePublic(keySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            return null;
+        }
     }
 
     public String getPublicKey() {
