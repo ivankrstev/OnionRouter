1d6986807ed2e1c1a94313361c48d9d4d2ce6661
Ivan Krstev <ivankrstev246@gmail.com>
2024-05-24 02:07:27 +0200

    Added a socket handler of a relay which receives the encrypted messages

---
+++ src/main/java/org/onionrouter/network/RelayNodeSocketHandler.java
@@ -0,0 +1,34 @@
+package org.onionrouter.network;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.security.PrivateKey;
+
+public class RelayNodeSocketHandler extends Thread {
+    private final Socket clientSocket;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final PrivateKey privateKey;
+
+    public RelayNodeSocketHandler(Socket socket, PrivateKey privateKey) {
+        this.clientSocket = socket;
+        this.privateKey = privateKey;
+    }
+
+    public void run() {
+        try (DataInputStream in = new DataInputStream(clientSocket.getInputStream())) {
+            String relayNodeMessageString = in.readUTF();
+            System.out.println("Received an encrypted message: " + relayNodeMessageString);
+
+            RelayNodeMessage relayNodeMessage = objectMapper.readValue(relayNodeMessageString, RelayNodeMessage.class);
+            DecryptedPayload decryptedMessage = relayNodeMessage.decryptAndDeserializeEncryptedMessage(privateKey);
+            System.out.println("Decrypted message: " + decryptedMessage);
+
+            new RelaySocketClient(decryptedMessage.getDestination()).forwardMessage(decryptedMessage.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error while reading the message from the client!");
+        }
+    }
+}
