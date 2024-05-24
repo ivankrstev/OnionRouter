4fee0fe1f47746c24321ad8d074975cc413658d6
Ivan Krstev <ivankrstev246@gmail.com>
2024-05-24 02:14:03 +0200

    Removed the router status & port generating due to hard-coded tor nodes

--- src/main/java/org/onionrouter/torserver/TorNodesServer.java
+++ src/main/java/org/onionrouter/torserver/TorNodesServer.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.onionrouter.nodes.RouterStatus;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,20 +11,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
-
-import static org.onionrouter.nodes.RouterStatus.ENTRY;
-import static org.onionrouter.nodes.RouterStatus.MIDDLE;
-import static org.onionrouter.nodes.RouterStatus.EXIT;
 
 public class TorNodesServer extends AbstractHandler {
     // We use connectedNodes to store the nodes of the tor network, so we can access their public keys, addresses(ports) and status
     private static final List<TorRouterInfoObject> connectedNodes = Collections.synchronizedList(new ArrayList<>());
     // Object mapper for writing/reading json values
     private final ObjectMapper objectMapper = new ObjectMapper();
-    // Range of ports for the server to generate from, to prevent duplicates
-    private static final int MIN_PORT = 6000;
-    private static final int MAX_PORT = 15000;
 
     @Override
     public void handle(String s, Request request, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
@@ -85,33 +76,6 @@ public class TorNodesServer extends AbstractHandler {
             shuffledNodes.add(entryNode);
             shuffledNodes.addAll(middleAndExitNodes);
             return shuffledNodes;
-        }
-    }
-
-    private static int generateNonExistingPort() throws Exception {
-        if (MAX_PORT - MIN_PORT + 1 == connectedNodes.size())
-            throw new Exception("No more free ports in the specific range");
-        Random random = new Random();
-        int generatedPort;
-        do {
-            generatedPort = MIN_PORT + random.nextInt(MAX_PORT - MIN_PORT + 1);
-            synchronized (connectedNodes) {
-                int finalGeneratedPort = generatedPort;
-                System.out.println("Generated port: " + generatedPort);
-                if (connectedNodes.stream().noneMatch(node -> node.getPort() == finalGeneratedPort))
-                    return generatedPort;
-            }
-        } while (true);
-    }
-
-    private static RouterStatus generateRouterStatus() {
-        Random random = new Random();
-        synchronized (connectedNodes) {
-            if (connectedNodes.stream().noneMatch(node -> node.getStatus() == ENTRY))
-                return ENTRY;
-            else if (connectedNodes.stream().noneMatch(node -> node.getStatus() == EXIT))
-                return EXIT;
-            else return random.nextBoolean() ? MIDDLE : EXIT;
         }
     }
 
