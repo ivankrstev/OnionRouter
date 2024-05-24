187deae59f7f9df9fc29c16e8c3c1630394fcbf6
Ivan Krstev <ivankrstev246@gmail.com>
2024-05-24 02:02:00 +0200

    Removed the http server & added socket server in final destination node

--- src/main/java/org/onionrouter/network/MessageReceiver.java
+++ src/main/java/org/onionrouter/network/MessageReceiver.java
@@ -1,53 +1,26 @@
 package org.onionrouter.network;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MessageReceiver {
     public static void main(String[] args) throws Exception {
-        Server server = new Server();
-
-        ServerConnector serverConnector = new ServerConnector(server);
-        serverConnector.setHost("onion-router-receiver");
-        serverConnector.setPort(8081);
-        server.addConnector(serverConnector);
-
-        ServletContextHandler handler = new ServletContextHandler();
-        handler.setContextPath("/");
-        handler.addServlet(new ServletHolder(new MessageReceiverServlet()), "/");
-        server.setHandler(handler);
-
-        server.start();
-        server.join();
-    }
-
-    private static class MessageReceiverServlet extends HttpServlet {
-        @Override
-        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-            System.out.println("MessageReceiver: Request received!");
-
-            // Read the request body
-            StringBuilder requestBody = new StringBuilder();
-            String line;
-            try (BufferedReader reader = req.getReader()) {
-                while ((line = reader.readLine()) != null)
-                    requestBody.append(line).append('\n');
+        //    Rename this(InboxServer or InboxProcessor)
+        try (ServerSocket inboxSocket = new ServerSocket(8080)) {
+            System.out.println("Inbox server started on port 8080");
+            while (true) {
+                try (Socket clientSocket = inboxSocket.accept();
+                     DataInputStream in = new DataInputStream(clientSocket.getInputStream())) {
+                    String message = in.readUTF();
+                    System.out.println("Received message in inbox: " + message);
+                } catch (IOException e) {
+                    System.out.println("Error while accepting the client socket!");
+                }
             }
-
-            // Logging the message
-            System.out.println("Received a message: " + requestBody);
-
-            // Sending response to the client
-            resp.setContentType("text/plain");
-            resp.getWriter().println("Message received successfully!");
+        } catch (IOException e) {
+            System.out.println("Error while creating the inbox socket!");
         }
     }
 }
