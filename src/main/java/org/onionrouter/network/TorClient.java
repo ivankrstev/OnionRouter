413c9e4728b6c8fcecb965562d0db7acfb40ab64
Ivan Krstev <ivankrstev246@gmail.com>
2024-05-24 02:10:46 +0200

    Added the tor client which starts the tor tunnel with layers encryption & sending them as message

---
+++ src/main/java/org/onionrouter/network/TorClient.java
@@ -0,0 +1,47 @@
+package org.onionrouter.network;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.onionrouter.security.AES;
+import org.onionrouter.security.RSA;
+import org.onionrouter.torserver.TorRouterInfoObject;
+
+import javax.crypto.SecretKey;
+import java.util.List;
+
+public class TorClient {
+    private final String torNodesServerAddress;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public TorClient(String torServerAddress) {
+        this.torNodesServerAddress = torServerAddress;
+    }
+
+    public void sendMessage(String message, String destination) throws Exception {
+        TorRouterInfoObject[] torNodes = getTorNodesFromServer();
+        if (torNodes == null || torNodes.length == 0)
+            return;
+        for (int i = torNodes.length - 1; i >= 0; i--) {
+            SecretKey AESKey = AES.generateKey();
+            DecryptedPayload decryptedPayloadObject = new DecryptedPayload(message, destination);
+            String encryptedMessageJson = objectMapper.writeValueAsString(decryptedPayloadObject);
+            encryptedMessageJson = AES.encrypt(encryptedMessageJson, AESKey);
+            RelayNodeMessage relayNodeMessage = new RelayNodeMessage(encryptedMessageJson, RSA.encrypt(AESKey, torNodes[i].parseAndGetPublicKey()));
+            message = objectMapper.writeValueAsString(relayNodeMessage);
+            destination = torNodes[i].getAddress();
+        }
+        new RelaySocketClient(destination).forwardMessage(message);
+    }
+
+    private TorRouterInfoObject[] getTorNodesFromServer() {
+        try {
+            String jsonResponse = new HttpMessageDispatcher().sendGetRequest(torNodesServerAddress + "/get-nodes");
+            // Deserialize the json response to an array of TorRouterInfoObject
+            List<TorRouterInfoObject> nodes = objectMapper.readValue(jsonResponse, new TypeReference<>() {
+            });
+            return nodes.toArray(new TorRouterInfoObject[0]);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
