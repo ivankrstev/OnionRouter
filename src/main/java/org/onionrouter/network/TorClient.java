5916b092cf705c656cc0d2e90645328bd2c41c05
Ivan Krstev <ivankrstev246@gmail.com>
2024-05-25 04:01:18 +0200

    Removed socket client and handler & adjusted the tor client to encrypt with the new payloads

--- src/main/java/org/onionrouter/network/TorClient.java
+++ src/main/java/org/onionrouter/network/TorClient.java
@@ -2,8 +2,8 @@ package org.onionrouter.network;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.onionrouter.network.relay.EncryptedPayloadWithFlag;
 import org.onionrouter.network.relay.RelayNodeMessage;
-import org.onionrouter.network.relay.RelaySocketClient;
 import org.onionrouter.security.AES;
 import org.onionrouter.security.RSA;
 import org.onionrouter.torserver.TorNodeInfo;
@@ -19,20 +19,24 @@ public class TorClient {
         this.torNodesServerAddress = torServerAddress;
     }
 
-    public void sendMessage(String message, String destination) throws Exception {
+    public void sendRequest(String destination, HttpPayload httpPayload) throws Exception {
         TorNodeInfo[] torNodes = getTorNodesFromServer();
         if (torNodes == null || torNodes.length == 0)
             return;
+        String message = objectMapper.writeValueAsString(httpPayload);
         for (int i = torNodes.length - 1; i >= 0; i--) {
             SecretKey AESKey = AES.generateKey();
-            DecryptedPayload decryptedPayloadObject = new DecryptedPayload(message, destination);
-            String encryptedMessageJson = objectMapper.writeValueAsString(decryptedPayloadObject);
-            encryptedMessageJson = AES.encrypt(encryptedMessageJson, AESKey);
-            RelayNodeMessage relayNodeMessage = new RelayNodeMessage(encryptedMessageJson, RSA.encrypt(AESKey, torNodes[i].parseAndGetPublicKey()));
+            String flag = i == torNodes.length - 1 ? "DESTINATION" : "RELAY";
+            EncryptedPayloadWithFlag encryptedPayloadWithFlag = new EncryptedPayloadWithFlag(message, flag, destination);
+            String encryptedPayloadWithFlagJSONString = objectMapper.writeValueAsString(encryptedPayloadWithFlag);
+            encryptedPayloadWithFlagJSONString = AES.encrypt(encryptedPayloadWithFlagJSONString, AESKey);
+            RelayNodeMessage relayNodeMessage = new RelayNodeMessage(encryptedPayloadWithFlagJSONString, RSA.encrypt(AESKey, torNodes[i].parseAndGetPublicKey()));
             message = objectMapper.writeValueAsString(relayNodeMessage);
             destination = torNodes[i].getAddress();
         }
-        new RelaySocketClient(destination).forwardMessage(message);
+        System.out.println("First relay: " + destination);
+        System.out.println("Sending message: " + message);
+        System.out.println("Response from tor client: " + new HttpMessageDispatcher().sendPostRequest(destination, message));
     }
 
     private TorNodeInfo[] getTorNodesFromServer() {
